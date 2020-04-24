d248be042c9d819550e9e4f6f07685b4a955661d
Jonatas Adams da Silva <jonatas.silva@hbsis.com.br>
2020-04-24 08:49:44 -0300

    Localizador de Devs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://jonatas:jonatas@cluster0-wk2kr.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
